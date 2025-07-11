201f4948535a819618e09030ea06abf62e922d7e
Patrignani Filippo <filippo.patrignani2@studio.unibo.it>
2025-07-11 19:07:18 +0200

    top 10 instead of all fixed

--- app/src/main/java/db_project/model/DBModel.java
+++ app/src/main/java/db_project/model/DBModel.java
@@ -348,4 +348,45 @@ public final class DBModel implements Model {
     public List<Optional<VideoGames>> getTop10GamesByGenre(String genre) {
         return VideoGames.DAO.getTop10GamesByGenre(connection, genre);
     }
+    
+    // Implementation of methods to get ALL games sorted (not just top 10)
+    @Override
+    public List<Optional<VideoGames>> getAllNewestGames() {
+        return VideoGames.DAO.getAllNewestGames(connection);
+    }
+    
+    @Override
+    public List<Optional<VideoGames>> getAllOldestGames() {
+        return VideoGames.DAO.getAllOldestGames(connection);
+    }
+    
+    @Override
+    public List<Optional<VideoGames>> getAllHighestRatedGames() {
+        return VideoGames.DAO.getAllHighestRatedGames(connection);
+    }
+    
+    @Override
+    public List<Optional<VideoGames>> getAllLowestRatedGames() {
+        return VideoGames.DAO.getAllLowestRatedGames(connection);
+    }
+    
+    @Override
+    public List<Optional<VideoGames>> getAllMostExpensiveGames() {
+        return VideoGames.DAO.getAllMostExpensiveGames(connection);
+    }
+    
+    @Override
+    public List<Optional<VideoGames>> getAllCheapestGames() {
+        return VideoGames.DAO.getAllCheapestGames(connection);
+    }
+    
+    @Override
+    public List<Optional<VideoGames>> getAllMostSoldGames() {
+        return VideoGames.DAO.getAllMostSoldGames(connection);
+    }
+    
+    @Override
+    public List<Optional<VideoGames>> getAllGamesByGenre(String genre) {
+        return VideoGames.DAO.getAllGamesByGenre(connection, genre);
+    }
 }
